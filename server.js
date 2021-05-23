bc0f8e75ce746597aa4baf16d80d586a0e887b2b
anitkumarsingh <anikumarsingh99@gmail.com>
2021-05-23 18:46:20 +0530

    created bootcamp routes

--- server.js
+++ server.js
@@ -4,8 +4,42 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
-
 const PORT = process.env.PORT;
+// Routes
+app.get('/api/v1/bootcamp', (req, res) => {
+  res.json({
+    success: true,
+    msg: 'Show all bootcamps'
+  });
+});
+
+app.get('/api/v1/bootcamp/:id', (req, res) => {
+  res.json({
+    success: true,
+    msg: 'Get single bootcamps'
+  });
+});
+
+app.post('/api/v1/bootcamp', (req, res) => {
+  res.json({
+    success: true,
+    msg: 'Create a bootcamps'
+  });
+});
+
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+  res.json({
+    success: true,
+    msg: `Update a bootcamps ${req.params.id}`
+  });
+});
+
+app.delete('/api/v1/bootcamp/:id', (req, res) => {
+  res.json({
+    success: true,
+    msg: `Delete a bootcamps ${req.params.id}`
+  });
+});
 
 app.listen(PORT, () =>
   console.log(
