2cd36390429e9500385aeaacd4f58d7f92386324
anitkumarsingh <anikumarsingh99@gmail.com>
2021-05-29 12:41:00 +0530

    duplicate and other mongoose error handling

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,13 +3,26 @@ const errorResponse = require('../utils/errorResponse');
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
+
+  // Mongoose bad ObjectId error
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new errorResponse(message, 404);
   }
-  res.status(error.statusCode).json({
+  // Mongoose duplicate error
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new errorResponse(message, 400);
+  }
+  // Mongoose ValidationError
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((i) => i.message);
+    error = new errorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    msg: error.message
+    msg: error.message || 'Server error'
   });
 };
 
