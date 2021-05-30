7d146e1b3da906b674c9e7644c02b90b828736d6
anitkumarsingh <anikumarsingh99@gmail.com>
2021-05-30 11:35:04 +0530

    course model and data insertion

--- seeder.js
+++ seeder.js
@@ -8,10 +8,15 @@ dotenv.config({ path: './config/config.env' });
 
 // Load model
 const Bootcamp = require('./models/bootcamp');
+const Course = require('./models/courses');
 
 // Load bootcamp json file
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/mock/bootcamps.json`, 'utf-8')
+);
+
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/mock/courses.json`, 'utf-8')
 );
 
 // Connect to database
@@ -25,6 +30,7 @@ mongoose.connect(process.env.MONGO_DB_URL, {
 const dataImporter = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data inserted into database'.green.inverse);
   } catch (error) {
     console.log('error', error);
@@ -34,6 +40,7 @@ const dataImporter = async () => {
 const dataDestroyer = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('All data deleted from database'.red.inverse);
   } catch (error) {
     console.log('error', error);
