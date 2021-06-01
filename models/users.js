da85a015f0a6591abb8f6111cbd6b42f4983718b
anitkumarsingh <anikumarsingh99@gmail.com>
2021-06-01 09:43:28 +0530

    user model creation

---
+++ models/users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add valid email'],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please enter valid email'
+    ]
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: [true, 'Please add password'],
+    select: false
+  },
+  role: {
+    type: String,
+    required: [true, 'Please select role'],
+    enum: ['Admin', 'User', 'Publiser'],
+    default: 'User'
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
