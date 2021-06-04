23221ebc16523d6ea7c777c370f0760e84554a24
anitkumarsingh <anikumarsingh99@gmail.com>
2021-06-04 17:55:45 +0530

    course ownership

--- models/courses.js
+++ models/courses.js
@@ -35,6 +35,11 @@ const coursesSchema = mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'Bootcamp',
     required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
   }
 });
 
